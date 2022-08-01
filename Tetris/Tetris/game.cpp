7b6ce69264d9a95647357b009be9cc5bc4e30b6e
jslee99 <junsub_lee@naver.com>
2022-08-02 00:18:06 +0900

    기본뺘대

--- Tetris/Tetris/game.cpp
+++ Tetris/Tetris/game.cpp
@@ -2,14 +2,101 @@
 
 using namespace std;
 
-void Game::downBlock() {
-	c--;
+void Game::check_turn_change() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (BLOCK[cur_BLOCK][cur_rotation][i][j] == 2) {
+				if (table[r + i + 1][c + j] == 1 || table[r + i + 1][c + j] == 3) turn_change = true;
+			}
+		}
+	}
 }
 
-void Game::gameInit() {
+void Game::convert_active_block_to_static_block(){
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (BLOCK[cur_BLOCK][cur_rotation][i][j] == 2) table[r + i][c + j] = 3;
+		}
+	}
+}
+
+bool Game::get_turn_change(){
+	return turn_change;
+}
+
+void Game::recontrolTable(){
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (BLOCK[cur_BLOCK][cur_rotation][i][j] == 2) table[r + i][c + j] = 0;
+		}
+	}
+}
+
+bool Game::get_game_over()
+{
+	return game_over;
+}
+
+void Game::downBlock() {
+	r++;
+}
+
+void Game::chage_block_q(){
+	cur_BLOCK = next_block_q.front();
+	cur_rotation = 0;
+	next_block_q.pop();
+
+
+	//test
+	//next_block_q.push(rand() % 7);
+	next_block_q.push(0);
+}
+
+void Game::check_gameover_at_change_turn(){
+	this->reset_RC();
+	bool flag = false;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (BLOCK[cur_BLOCK][cur_rotation][i][j] == 2) {
+				while (r + i < 1)r++;
+				flag = true;
+				break;
+			}
+		}
+		if (flag) break;
+	}
+
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (BLOCK[cur_BLOCK][cur_rotation][i][j] == 2) {
+				if (table[r + i][c + j] == 3) game_over = true;
+			}
+		}
+	}
+
+	turn_change = false;
+}
+
+void Game::reset_RC(){
 	r = -1;
-	c = 7;
-	next_turn = true;
+	c = 4;
+}
+
+
+
+void Game::gameInit() {
+	turn_change = true;
+	game_over = false;
+	
+
+	srand((unsigned int)time(NULL));
+	for (int i = 0; i < 7; i++) {
+		//test
+		//next_block_q.push(rand() % 7);
+		next_block_q.push(0);
+	}
+
 	for (int i = 0; i < TABLE_R; i++) {
 		for (int j = 0; j < TABLE_C; j++) {
 			if (is_wall_position(i, j))table[i][j] = 1;
@@ -35,31 +122,10 @@ void Game::drawTable(){
 	}
 }
 
-
-
-
-
 void Game::controlTable() {
-	for (int i = 1; i < TABLE_R - 1; i++) {
-		for (int j = 1; j < TABLE_C - 1; j++) {
-			if (table[i][j] == 3)table[i][j] = 3;
-			else table[i][j] = 0;
-		}
-	}
-
-	for (int i = 0; i < 4; i++) {//꾸겨넣기
+	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			if (cur_BLOCK[i][j] == 2) {
-				while (r + i < 1) r++;
-				while (c + j < 1) c++;
-				while (c + j > TABLE_C - 2) c--;
-			}
-		}
-	}
-
-	for (int i = 0; i < 4; i++) {//game_over check
-		for (int j = 0; j < 4; j++) {
-			if (table[r + i][c + j] == 2) game_over = true;
+			if (BLOCK[cur_BLOCK][cur_rotation][i][j] == 2)table[r + i][c + j] = 2;
 		}
 	}
 }
--- Tetris/Tetris/main.cpp
+++ Tetris/Tetris/main.cpp
@@ -5,29 +5,54 @@
 using namespace std;
 
 /*
+gameinit next_trun = true, 벽 설정
+
 if next_turn
-	change blockqueue
-	change cur_BLOCK
-	check can positioning cur_BLOCK at start point if cant game_over (일단 꾸겨넣고 3번과 겹치면 game_over)
+	change blockqueue change cur_BLOCK
+	check can positioning cur_BLOCK at start point if cant game_over (일단 꾸겨넣고(r만 조절함) 3번과 겹치면 game_over)
 	reset r, c
 else
-	roation감지
-	left_right 감지
-	control table // set value of table(꾸겨넣기(1,3을 모두 신경쓴다.)) , 불가능한 경우인지 확인(1번 또는 3번과 겹치면 game_over)),  (충돌감지)
+	roation감지, 변경(꾸겨넣기, 안되면(일단 1을 위주로 꾸겨넣고 그후에 3과 겹치게 되는 경우) 원래 상태로 돌려놓기)
+	left_right 감지(가능한지 check 가능하면 r변경)
+
+	control table // set value of table(별다른 오류감지 필요 없음)
 	draw table
-	check game_over // (next_turn == true && block position at first line)
-	c--
+	충돌감지(아래에 1이나 3이 있는지 여부, next_turn 설정)
+
+	
+	if(!turn_change)
+		c--
+	else
+		convert 2 to 3
+		combo check//아직 안함
 	sleep
 */
 int main(void) {
 	Game game = Game();
 	hideCursor();
 	game.gameInit();
-
 	while (1) {
-		system("cls");
-		game.controlTable();
-		game.drawTable();
-		Sleep(25);
+		if (game.get_turn_change()) {
+			game.chage_block_q();
+			game.check_gameover_at_change_turn();
+			if (game.get_game_over())break;			
+		}
+		else {
+			//rotation
+			//left_right
+			game.controlTable();
+			system("cls");
+			game.drawTable();
+			game.check_turn_change();
+
+			if (!game.get_turn_change()) {
+				game.recontrolTable();
+				game.downBlock();			
+				Sleep(50);
+			}
+			else {
+				game.convert_active_block_to_static_block();
+			}
+		}
 	}
 }
